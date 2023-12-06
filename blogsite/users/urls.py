555de286bdd553f5fea7335bfaac2ffe84cfd99c
4ntim4tter <4ntim4tter@gmail.com>
2023-12-06 19:40:13 +0100

    .

--- blogsite/users/urls.py
+++ blogsite/users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("login/", views.login_user, name="login_user"),
-    path("register/", views.register_user, name="register_user")
+    path("register/", views.register_user, name="register_user"),
+    path('create/', views.create_user, name="create_user")
 ]
--- blogsite/users/views.py
+++ blogsite/users/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 # Create your views here.
@@ -8,3 +10,9 @@ def login_user(request):
 
 def register_user(request):
     return render(request, 'users/register.html', {})
+
+def create_user(request):
+    user = User.objects.create_user(username=request.POST['username'],
+                                    password=request.POST['password'])
+    user.save()
+    return HttpResponse(request.POST['username'])
