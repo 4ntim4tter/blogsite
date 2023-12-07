5c80b1ff22686e7f29eac39bb4fa9174462643d1
4ntim4tter <4ntim4tter@gmail.com>
2023-12-07 17:15:21 +0100

    .

--- blogsite/users/urls.py
+++ blogsite/users/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from posts import views as home_views
 
 urlpatterns = [
     path("login/", views.login_user, name="login_user"),
     path("register/", views.register_user, name="register_user"),
-    path('create/', views.create_user, name="create_user")
+    path('create/', views.create_user, name="create_user"),
+    path('authenticate/', views.auth_user, name="auth_user"),
+    path('', home_views.PostsIndexView.as_view(), name="index")
 ]
--- blogsite/users/views.py
+++ blogsite/users/views.py
@@ -1,20 +1,30 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.contrib import messages
 
 # Create your views here.
-def login_user(request):
-    
+def login_user(request): 
     return render(request, 'users/login.html', {})
 
 def register_user(request):
     return render(request, 'users/register.html', {})
 
 def create_user(request):
-    user = User.objects.create_user(username=request.POST['username'],
-                                    password=request.POST['password'])
-    user.save()
-    logged_in = authenticate(username=request.POST['username'], password=request.POST['password'])
-    return HttpResponse(logged_in)
+    print(User.objects.filter(username=request.POST['username']))
+    if User.objects.filter(username=request.POST['username']):
+        messages.error(request, "User already exists.")
+        return redirect(register_user)
+    else:
+        user = User.objects.create_user(username=request.POST['username'],
+                                        password=request.POST['password'])
+        user.save()
+        login(request, user)
+        messages.success(request, "Registration Successful.")
+        return redirect('index')
+    
+def auth_user(request):
+    user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+    login(request, user)
+    messages.success(request, "Login Successful.")
+    return redirect('index')
