46c162fb0fa8601a47ca9298738f552e83009c6a
Anes <4ntim4tter@gmail.com>
2023-12-28 13:02:14 +0100

    .

--- blogsite/posts/urls.py
+++ blogsite/posts/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostsIndexView.as_view(), name="index")
+    path("", views.PostsIndexView.as_view(), name="index"),
+    path("post/<int:pk>/", views.SeePost.as_view(), name="see_post")
 ]
--- blogsite/posts/views.py
+++ blogsite/posts/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import redirect, render
 from django.views.generic import ListView
 
 from .models import Post
@@ -15,3 +18,12 @@ class PostsIndexView(ListView):
 
     def get_queryset(self):
         return Post.objects.order_by('-pub_date', '-id')
+
+class SeePost(ListView):
+    template_name = "posts/see_post.html"
+    context_object_name = "post"
+    model = Post
+
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        return render(request, 'posts/see_post.html', {'post':post})
