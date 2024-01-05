96e0fdd72d201c8f18f18b0647b2e5374eb4e7de
Anes <4ntim4tter@gmail.com>
2024-01-05 09:20:27 +0100

    .

--- blogsite/posts/models.py
+++ blogsite/posts/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,6 +12,12 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def comment_number(self):
+        return self.comment_set.count()
+
+    def likes(self):
+        return self.objects.annotate(num_of_likes=models.Count())
 
 class Comment(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,4 +28,17 @@ class Comment(models.Model):
     def __str__(self):
         comment =  self.username.username + ": " + self.text.rstrip()
         return comment
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    def __str__(self):
+        return self.user
     
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
