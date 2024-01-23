d248479caa55bf1cbe3ee0f45e54ebd421129b93
4ntim4tter <4ntim4tter@gmail.com>
2024-01-23 21:26:10 +0100

    .

--- blogsite/users/tests.py
+++ blogsite/users/tests.py
@@ -1,17 +1,21 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 
 # Create your tests here.
 from django.test import Client
 
 class TestViews(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+
     def test_auth_user(self):
-        client = Client()
-        response = client.post('/authenticate/', {'username':'sarmica', 'password':'123321'})
+        response = self.client.post('/authenticate/', {'username':'sarmica', 'password':'123321'})
         self.assertEqual(response.status_code, 200)
 
     def test_create_user(self):
-        client = Client()
-        client.post('/create/', {'username':'digimon22', 'password':'232312'})
+        self.client.post('/create/', {'username':'digimon22', 'password':'232312'})
         user = User.objects.get(username='digimon22')
         self.assertEqual(user.username, 'digimon22')
+
+    
